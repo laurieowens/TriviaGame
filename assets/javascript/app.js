d338e7f5468a29581770c49b14a4c0b011ea86be
Jake Owens <Jowens@Jakes-Air-3.attlocal.net>
2017-09-29 22:34:57 -0400

    update test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 var count=20;
-	choices=["Which one of these fruits has its seeds on the outside?","Which rule is the best way to solve a limit that gives an indeterminate form?","In the original 'Wizard of Oz Book' and the movie 'Wicked' these slippers are not “ruby” but this metallic color:","Which law states that we are constantly headed towards chaos?"];
+	choices=["Which one of these fruits has its seeds on the outside?","Which rule is the best way to solve a limit that gives an indeterminate form?","In the original 'Wizard of Oz Book' and the movie 'Wicked' these slippers are not “ruby” but this metallic color:","Which law states that we are constantly headed towards chas?"];
 	rightChoices=[];
 	wrongChoices=[];
 	noChoice=[];
